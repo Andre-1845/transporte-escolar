6ef648743d5f1efd9be1811a3b96c7e8a8df8f9b
Andre Andrino <andrebaumgratz@gmail.com>
2026-02-15 18:43:20 -0300

    feat: initial Laravel 12 SaaS structure with Sanctum, Spatie and multi-tenant school base

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
+
+
+    /**
+     * Define o guard padrão para permissões.
+     */
+    protected string $guard_name = 'web';
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +51,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
     }
 }
